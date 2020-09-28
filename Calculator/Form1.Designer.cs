595301302e17981796a4b5492832bfd033b01736
DoniyorJN <umidshoot@gmail.com>
2020-09-29 00:00:39 +0500

    init: prototype calculator

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.AccessibleName = "";
+            this.numberTextBox.Location = new System.Drawing.Point(41, 22);
+            this.numberTextBox.Multiline = true;
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(194, 20);
+            this.numberTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(41, 83);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(87, 48);
+            this.copyButton.TabIndex = 2;
+            this.copyButton.Text = "Copy Result";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.ForeColor = System.Drawing.Color.Maroon;
+            this.resultLabel.Location = new System.Drawing.Point(56, 50);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(167, 23);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(241, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(63, 119);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 146);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numberTextBox);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public double result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string getNumber = numberTextBox.Text;
+            result = getResults(getNumber);
+            resultLabel.Text = result.ToString();
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            if (resultLabel.Text == "")
+            {
+                MessageBox.Show("Nothing to copy :(");
+            }
+            else
+            {
+                Clipboard.SetText(resultLabel.Text);
+                MessageBox.Show("Result coped be happy use result with Ctrl+V");
+            }
+        }
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+            numberTextBox.Text = "";
+            numberTextBox.Focus();
+        }
+        private double getResults(string mathOperation)
+        {
+
+            var numberStack = new List<double>();
+            var operationStackAndBrace = new List<string>();
+            Dictionary<string, int> priorityOfMathematicalOperations = new Dictionary<string, int>()
+            {
+              {"+", 1}, {"-", 1}, {"*", 2}, {"/", 2}, {"^",3}, {"(", 0 }
+            };
+            string[] splitedMath = divideIntoParts(mathOperation);
+            int numberOfElements = operationStackAndBrace.Count;
+            int numberOfNumbers = numberStack.Count;
+            foreach (string operation in splitedMath)
+            {
+                numberOfElements = operationStackAndBrace.Count;
+                numberOfNumbers = numberStack.Count;
+
+                if (double.TryParse(operation, out double number))
+                {
+                    numberStack.Add(number);
+                }
+                else
+                {
+                    if (operation == "+" || operation == "-" || operation == "*" || operation == "/" ||
+                       operation == "^" || operation == "(")
+                    {
+                        if (operation == "(" || numberOfElements == 0)
+                        {
+                            operationStackAndBrace.Add(operation);
+                        }
+                        else
+                        {
+                            if (priorityOfMathematicalOperations[operation] >=
+                                priorityOfMathematicalOperations[operationStackAndBrace[numberOfElements - 1]])
+                            {
+                                operationStackAndBrace.Add(operation);
+                            }
+                            else if (numberOfNumbers > 1)
+                            {
+                                double num1 = numberStack[numberOfNumbers - 2], num2 = numberStack[numberOfNumbers - 1];
+                                if (operationStackAndBrace[numberOfElements - 1] == "+")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    operationStackAndBrace.Add(operation);
+                                    numberStack.Add(num1 + num2);
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "-")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    operationStackAndBrace.Add(operation);
+                                    numberStack.Add(num1 - num2);
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "*")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    operationStackAndBrace.Add(operation);
+                                    numberStack.Add(num1 * num2);
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "/")
+                                {
+                                    if (num2 == 0)
+                                    {
+                                        MessageBox.Show("Cannot divide zero");
+                                        return -1;
+                                    }
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    operationStackAndBrace.Add(operation);
+                                    numberStack.Add(num1 / num2);
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "^")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    operationStackAndBrace.Add(operation);
+                                    numberStack.Add(Math.Pow(num1, num2));
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Enter Correct value");
+                                return -1;
+                            }
+                        }
+
+                    }
+                    else if (operation == ")")
+                    {
+                        if (numberOfElements > 1 && operationStackAndBrace.Contains("("))
+                        {
+                            while (numberOfNumbers > 1 && numberOfElements > 1)
+                            {
+                                numberOfElements = operationStackAndBrace.Count;
+                                numberOfNumbers = numberStack.Count;
+                                double num1 = numberStack[numberOfNumbers - 2], num2 = numberStack[numberOfNumbers - 1];
+                                if (operationStackAndBrace[numberOfElements - 1] == "+")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    numberStack.Add(num1 + num2);
+                                    numberOfElements = operationStackAndBrace.Count;
+                                    numberOfNumbers = numberStack.Count;
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "-")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    numberStack.Add(num1 - num2);
+                                    numberOfElements = operationStackAndBrace.Count;
+                                    numberOfNumbers = numberStack.Count;
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "*")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    numberStack.Add(num1 * num2);
+                                    numberOfElements = operationStackAndBrace.Count;
+                                    numberOfNumbers = numberStack.Count;
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "/")
+                                {
+                                    if (num2 == 0)
+                                    {
+                                        MessageBox.Show("Cannot divide zero");
+                                        return -1;
+                                    }
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    numberStack.Add(num1 / num2);
+                                    numberOfElements = operationStackAndBrace.Count;
+                                    numberOfNumbers = numberStack.Count;
+                                }
+                                else if (operationStackAndBrace[numberOfElements - 1] == "^")
+                                {
+                                    numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    numberStack.Add(Math.Pow(num1, num2));
+                                    numberOfElements = operationStackAndBrace.Count;
+                                    numberOfNumbers = numberStack.Count;
+                                }
+                                if (operationStackAndBrace[numberOfElements - 1] == "(")
+                                {
+                                    operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                                    numberOfElements = operationStackAndBrace.Count;
+                                    numberOfNumbers = numberStack.Count;
+                                    break;
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Enter Correct value");
+                            return -1;
+                        }
+                    }
+                }
+            }
+            numberOfElements = operationStackAndBrace.Count;
+            numberOfNumbers = numberStack.Count;
+            if (numberOfElements + 1 == numberOfNumbers)
+            {
+                while (numberOfNumbers != 1)
+                {
+                    numberOfElements = operationStackAndBrace.Count;
+                    numberOfNumbers = numberStack.Count;
+                    double num1 = numberStack[numberOfNumbers - 2], num2 = numberStack[numberOfNumbers - 1];
+                    if (operationStackAndBrace[numberOfElements - 1] == "+")
+                    {
+                        numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                        operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                        numberStack.Add(num1 + num2);
+                        numberOfElements = operationStackAndBrace.Count;
+                        numberOfNumbers = numberStack.Count;
+                    }
+                    else if (operationStackAndBrace[numberOfElements - 1] == "-")
+                    {
+                        numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                        operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                        numberStack.Add(num1 - num2);
+                        numberOfElements = operationStackAndBrace.Count;
+                        numberOfNumbers = numberStack.Count;
+                    }
+                    else if (operationStackAndBrace[numberOfElements - 1] == "*")
+                    {
+                        numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                        operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                        numberStack.Add(num1 * num2);
+                        numberOfElements = operationStackAndBrace.Count;
+                        numberOfNumbers = numberStack.Count;
+                    }
+                    else if (operationStackAndBrace[numberOfElements - 1] == "/")
+                    {
+                        if (num2 == 0)
+                        {
+                            MessageBox.Show("Cannot divide zero");
+                            return -1;
+                        }
+                        numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                        operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                        numberStack.Add(num1 / num2);
+                        numberOfElements = operationStackAndBrace.Count;
+                        numberOfNumbers = numberStack.Count;
+                    }
+                    else if (operationStackAndBrace[numberOfElements - 1] == "^")
+                    {
+                        numberStack.RemoveRange(numberOfNumbers - 2, 2);
+                        operationStackAndBrace.RemoveAt(numberOfElements - 1);
+                        numberStack.Add(Math.Pow(num1, num2));
+                        numberOfElements = operationStackAndBrace.Count;
+                        numberOfNumbers = numberStack.Count;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter Correct value");
+                return -1;
+            }
+
+            return numberStack[0];
+        }
+
+        private string[] divideIntoParts(string input)
+        {
+            string pattern = @"([*()\^\/]|(?<!E)[\+\-])";
+            string[] substrings = Regex.Split(input, pattern);
+            return substrings;
+        }
+
+        
+    }
+}
